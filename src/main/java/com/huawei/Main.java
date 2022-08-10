3ea6423169f5f19e5601fe4cc10b07d1e3508580
zhangziqiang1 <35445530+zhangziqiang1@users.noreply.github.com>
2022-08-10 13:11:15 +0800

    Add files via upload

---
+++ src/main/java/com/huawei/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+ */
+
+package com.huawei;
+
+import com.huawei.util.StringUtil;
+
+/**
+ * Main
+ *
+ * @since 2022-08-10
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        StringUtil stringUtil = new StringUtil();
+        stringUtil.isHuawei();
+    }
+}
